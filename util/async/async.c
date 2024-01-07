c5717385486d3d906e5c13be1c812db6c052d8ae
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-01-07 04:09:19 +0200

    in debbug work, but not in usual run

--- util/async/async.c
+++ util/async/async.c
@@ -8,69 +8,86 @@
 
 #define POOL_SIZE 4
 static mtx_t mut[POOL_SIZE];
-static thrd_t pool[POOL_SIZE];
+static mtx_t man_m;
+static thrd_t thrd_pool[POOL_SIZE];
 static Queue *q = NULL;
 static bool work = false;
 static struct func_poll {
-    void **pool;
+    int (*pool[POOL_SIZE])(void *);
 } POOL;
 
 
 
 static int worker_0(void *func) {
+    puts("worker 0 start");
     mtx_lock(&mut[0]);
-    void *(*job) = func;
-    if (job) {}
+    void *(*job)(void *) = func;
+    int b = 1;
+    if (job(&b)) {}
     puts("Worker 0 done");
     mtx_unlock(&mut[0]);
     return 0;
 }
 static int worker_1(void *func) {
+    puts("worker 1 start");
     mtx_lock(&mut[1]);
-    void *(*job) = func;
-    if (job) {}
+    void *(*job)(void *) = func;
+    int b = 2;
+    if (job(&b)) {}
     puts("Worker 1 done");
     mtx_unlock(&mut[1]);
     return 0;
 }
 static int worker_2(void *func) {
+    puts("worker 2 start");
     mtx_lock(&mut[2]);
-    void *(*job) = func;
-    if (job) {}
+    void *(*job)(void *) = func;
+    int b = 3;
+    if (job(&b)) {}
     puts("Worker 2 done");
     mtx_unlock(&mut[2]);
     return 0;
 }
 static int worker_3(void *func) {
+    puts("worker 3 start");
     mtx_lock(&mut[3]);
-    void *(*job) = func;
-    if (job) {}
+    void *(*job)(void *) = func;
+    int b = 3;
+    if (job(&b)) {}
     puts("Worker 3 done");
     mtx_unlock(&mut[3]);
     return 0;
 }
 
 static void init_pool() {
-    POOL.pool = (void **) malloc(sizeof(void *) * POOL_SIZE);
-    POOL.pool[0] = &worker_0;
-    POOL.pool[1] = &worker_1;
-    POOL.pool[2] = &worker_2;
-    POOL.pool[3] = &worker_3;
+    POOL.pool[0] = worker_0;
+    POOL.pool[1] = worker_1;
+    POOL.pool[2] = worker_2;
+    POOL.pool[3] = worker_3;
 }
 
 
 
 static int _thread_manage(void *man) {
     puts("Thred manager init");
+    // pool[0] = (thrd_t) malloc(sizeof(thrd_t));
+    // pool[1] = (thrd_t) malloc(sizeof(thrd_t));
+    // pool[2] = (thrd_t) malloc(sizeof(thrd_t));
+    // pool[3] = (thrd_t) malloc(sizeof(thrd_t));
+    // thrd_t pool[POOL_SIZE];
+    int cur_worker = 0;
     while(work) {
-        int cur_worker = 0;
         if (get_size(q) == 0) {
-            thrd_sleep(&(struct timespec){.tv_nsec = 2000}, NULL);
+            thrd_sleep(&(struct timespec){.tv_nsec = 20000}, NULL);
             printf("I am sleep");
         }
         while(get_size(q) != 0) {
-            thrd_create(&pool[cur_worker], POOL.pool[cur_worker], queue_get(q));
-            cur_worker += cur_worker >= 4 ? -4 : 1;
+            // mtx_trylock(&mut[cur_worker]);
+            thrd_create(&thrd_pool[cur_worker], POOL.pool[cur_worker], queue_get(q));
+            cur_worker ++;
+            if (cur_worker >= 4) {
+                cur_worker = 0;
+            }
         }
     }
     puts("Thred manager done");
@@ -82,18 +99,20 @@ void _thread_prepare(void *func) {
     if (q == NULL) q = queue_create();
     queue_add(q, func);
     if (work == false) {
-        thrd_t man;
+        thrd_t man = {0};
         work = true;
-        thrd_create(&man, &_thread_manage, &man);
+        init_pool();
+        thrd_create(&man, _thread_manage, NULL);
+        // _thread_manage(NULL);
     }
 }
 
 void _finish_work() {
     work = false;
     for(int i = 0; i < POOL_SIZE; i++) {
-        thrd_join(pool[i], 0);
+        thrd_join(thrd_pool[i], 0);
+        printf("join %i thread\n", i);
     }
-    free(POOL.pool);
     free(q);
     printf("Finish work\n");
 }
--- util/async/main.c
+++ util/async/main.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include "async.h"
 
-void print1(int a) {
-    printf("%i\n", a);
+void *print1(void *a) {
+    for(int i = 0; i < 1000; i++)
+        fprintf(stdout, "%i", *(int *) a);
+    puts("");
+    return NULL;
 }
-void print2(int a) {
-    printf("%i\n", a);
+void *print2(void *a) {
+    for(int i = 0; i < 1000; i++)
+        printf("%i", *(int *) a);
+    puts("");
+    return NULL;
 }
-void print3(int a) {
-    printf("%i\n", a);
+void *print3(void *a) {
+    for(int i = 0; i < 1000; i++)
+        printf("%i", *(int *) a);
+    puts("");
+    return NULL;
 }
-void print4(int a) {
-    printf("%i\n", a);
+void *print4(void *a) {
+    for(int i = 0; i < 1000; i++)
+        printf("%i", *(int *) a);
+    puts("");
+    return NULL;
 }
 
 
 
 int main() {
-    printf("Start job");
+    printf("Start job\n");
 
+    async(&print1);
+    async(&print2);
+    async(&print3);
+    async(&print4);
     async(&print1);
     async(&print2);
     async(&print3);
