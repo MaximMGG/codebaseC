f73c566c3b20204d06e9df719612f33248c7e199
maximhrunenko <maxhrunenko@gmail.com>
2024-02-16 16:50:45 +0200

    add multithread safety and new type of getting memory chunks

--- alloc/allocator.c
+++ alloc/allocator.c
@@ -3,17 +3,20 @@
 static int allocator_error = 0;
 
 #define byte char
+typedef byte bool;
+#define true 1
+#define false 0
 
 static void allocator_mem_set(void *dest, byte mem, unsigned int size) {
-    byte *dp = (byte *) dest;
-
+    byte _Atomic *dp = (byte _Atomic*) dest;
     for(int i = 0; i < size; i++) {
         *(dp++) = mem;
     }
 }
+
 static void allocator_mem_cpy(void *dest, void *source, unsigned int size) {
-    byte *dp = (byte *) dest;
-    byte *sp = (byte *) source;
+    byte _Atomic *dp = (byte _Atomic*) dest;
+    byte _Atomic *sp = (byte _Atomic*) source;
 
     for(int i = 0; i < size; i++) {
         *(dp++) = *(sp++);
@@ -41,7 +44,6 @@ _Noreturn void allocator_error_print() {
             fprintf(stderr, "ALLOCATOR: size is bigger then allocator->mem_left\n");
         }
     }
-
     exit(1);
 }
 
@@ -62,21 +64,48 @@ allocator *allocator_alloc(size_t size) {
     return a;
 }
 
+static void *al_get_chunk(allocator *al, size_t size) {
+    mtx_lock(&al->allocator_mutex);
+    unsigned int count = 0;
+    byte ref = 0xdc;
+    byte *memp = al->cur_pointer;
+    for(int i = 0; i < al->size; i++) {
+        if (*memp == ref) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count == size) {
+            return memp;
+        }
+    }
+    mtx_unlock(&al->allocator_mutex);
+    return NULL;
+}
+
 void *al_get_mem(allocator *al, size_t size) {
+    mtx_lock(&al->allocator_mutex);
     if (size > al->size) {
         allocator_error = ALLOCATOR_OVERSIZE_ERROR;
+        mtx_unlock(&al->allocator_mutex);
         allocator_error_print();
         return null;
     }
     if (size > al->mem_left) {
         allocator_error = ALLOCATOR_LEFTMEM_ERROR;
+        mtx_unlock(&al->allocator_mutex);
         allocator_error_print();
         return null;
     }
-    void *p = al->cur_pointer + (al->size - al->mem_left);
+    void *p = al_get_chunk(al, size);
+    if (p == null) {
+        allocator_error = ALLOCATOR_NOT_FULLSIZE_CHUNK;
+        mtx_unlock(&al->allocator_mutex);
+        return null;
+    }
     al->mem_left -= size;
     allocator_mem_set(p, 0, size);
-
+    mtx_unlock(&al->allocator_mutex);
     return p;
 }
 
@@ -84,14 +113,17 @@ void *al_get_strmem(allocator *al, char *str) {
     if (str == null) {
         return null;
     }
+    mtx_lock(&al->allocator_mutex);
     unsigned int size = allocator_strlen(str);
     if (size > al->size) {
         allocator_error = ALLOCATOR_OVERSIZE_ERROR;
+        mtx_unlock(&al->allocator_mutex);
         allocator_error_print();
         return null;
     }
     if (size > al->mem_left) {
         allocator_error = ALLOCATOR_LEFTMEM_ERROR;
+        mtx_unlock(&al->allocator_mutex);
         allocator_error_print();
         return null;
     }
@@ -100,12 +132,34 @@ void *al_get_strmem(allocator *al, char *str) {
     allocator_mem_cpy(p, str, size);
     char *sp = p;
     sp[size] = '\0';
+    mtx_unlock(&al->allocator_mutex);
 
     return p;
 }
 
 
-void al_free(allocator* al) {
+static bool is_memory_in_allocator(allocator *al, void *p){
+    return  p >= al->cur_pointer && 
+            p <= al->cur_pointer + al->size ? 
+            true : false;
+}
+
+//0xdc
+ALLOCATOR_ERROR al_free(allocator* al, void *p, size_t size) {
+    mtx_lock(&al->allocator_mutex);
+    if (is_memory_in_allocator(al, p)) {
+        allocator_mem_set(p, 0xdc, size);
+        p = null;
+        al->mem_left += size;
+        mtx_unlock(&al->allocator_mutex);
+        return ALLOCATOR_OK;
+    } else {
+        mtx_unlock(&al->allocator_mutex);
+        return ALLOCATOR_NOT_ALLOC_ERROR;
+    }
+}
+
+void al_dealloc(allocator *al) {
     free(al->cur_pointer);
     al->cur_pointer = null;
     al->size = 0;
@@ -131,3 +185,4 @@ char *al_get_error() {
     }
     return "OK";
 }
+
--- alloc/allocator.h
+++ alloc/allocator.h
@@ -9,19 +9,23 @@ typedef struct {
     void *cur_pointer;
     size_t size;
     size_t mem_left;
+    mtx_t allocator_mutex;
 } allocator;
 
 typedef enum {
+    ALLOCATOR_OK = 0,
     ALLOCATOR_MALLOC_ERROR = 1, 
     ALLOCATOR_LEFTMEM_ERROR = 2,
-    ALLOCATOR_OVERSIZE_ERROR = 3
-
+    ALLOCATOR_OVERSIZE_ERROR = 3,
+    ALLOCATOR_NOT_ALLOC_ERROR = 4,
+    ALLOCATOR_NOT_FULLSIZE_CHUNK = 5,
 } ALLOCATOR_ERROR;
 
 allocator *allocator_alloc(size_t size);
 void *al_get_mem(allocator *al, size_t size);
 void *al_get_strmem(allocator *al, char *str);
-void al_free(allocator* al);
+ALLOCATOR_ERROR al_free(allocator* al, void *pointer, size_t size);
+void al_dealloc(allocator *al);
 char *al_get_error();
 
 #endif 
